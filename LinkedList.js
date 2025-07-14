fa45d092b4294f701d8b3a84d22817ad3d882ee2
Timothy Obosi <tobosi@britam.com>
2025-07-14 15:31:39 +0300

    Linked List

--- LinkedList.js
+++ LinkedList.js
@@ -6,5 +6,40 @@ class Node{
 }
 
 class LinkedList{
-    
+    constructor(){
+        this.head = null;
+    }
+
+    add(data){
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next){
+                current = current.next;
+            }
+            current.next = newNode;            
+        }
+    }
+
+    print(){
+        let current = this.head;
+        const result = [];
+        while (current) {
+            result.push(current.data);
+            current = current.next;
+        }
+        return result.join(' → ');
+    }
+
+    toArray(){
+        let current = this.head;
+        const result =[];
+        while (current) {
+            result.push(current.data);
+            current = current.next;            
+        }        
+        return result;
+    }
 }
